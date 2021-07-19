fb2a59042fe90011a0d2e8aed8d7fe88bbe0236e
Merwa95 <merwamanssar@gmail.com>
2021-07-19 04:16:20 +0200

    "add Session_Stateless"

--- src/main/java/com/merwa_manssar/app/ws/security/WebSecurity.java
+++ src/main/java/com/merwa_manssar/app/ws/security/WebSecurity.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import com.merwa_manssar.app.ws.services.UserService;
@@ -35,7 +36,10 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 		.permitAll()
 		.anyRequest().authenticated()
 		.and()
-		.addFilter(getAuthenticationFilter());
+		.addFilter(getAuthenticationFilter())
+		.addFilter(new AutorizationFilter(authenticationManager()))
+		.sessionManagement()
+		.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
 	protected AuthenticationFilter getAuthenticationFilter() throws Exception {
