aea50f1a94bda2e6be220836bdb9960fa9713e79
Merwa95 <merwamanssar@gmail.com>
2021-07-19 04:15:41 +0200

    "Add Autorization Filter"

---
+++ src/main/java/com/merwa_manssar/app/ws/security/AutorizationFilter.java
@@ -0,0 +1,70 @@
+package com.merwa_manssar.app.ws.security;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.core.log.LogMessage;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import io.jsonwebtoken.Jwts;
+
+public class AutorizationFilter extends BasicAuthenticationFilter {
+
+	public AutorizationFilter(AuthenticationManager authenticationManager) {
+		super(authenticationManager);
+		// TODO Auto-generated constructor stub
+	}
+
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+
+		String header = request.getHeader(SecurityConstants.HEADER_STRING);
+
+		if (header == null || !header.startsWith(SecurityConstants.TOKEN_PREFIX)) {
+			chain.doFilter(request, response);
+			return;
+		}
+
+			UsernamePasswordAuthenticationToken authRequest = getAuthentication(request);
+			if (authRequest == null) {
+				this.logger.trace("Did not process authentication request since failed to find ");
+				chain.doFilter(request, response);
+				return;
+			}
+			
+			 SecurityContextHolder.getContext().setAuthentication(authRequest);
+	        chain.doFilter(request, response);
+	}
+
+	private UsernamePasswordAuthenticationToken getAuthentication(HttpServletRequest request) {
+		String token = request.getHeader(SecurityConstants.HEADER_STRING);
+
+		if (token != null) {
+
+			token = token.replace(SecurityConstants.TOKEN_PREFIX, "");
+			// get idUser
+			String user = Jwts.parser().setSigningKey(SecurityConstants.TOKEN_SECRET)
+					.parseClaimsJws(token).getBody()
+					.getSubject();
+
+			if (user != null) {
+				return new UsernamePasswordAuthenticationToken(user, null, new ArrayList<>());
+			}
+
+			return null;
+		}
+
+		return null;
+	}
+
+}
